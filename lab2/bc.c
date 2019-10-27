0ae4418ba4ee03307b1e07ece4c98e3c1eb0ccda
niconiconini <yzxsyh123@126.com>
2019-10-26 20:29:29 -0700

    added a filee

---
+++ lab2/bc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int  n,count=0,x,i;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+         x=n%2;
+         n=(int)(n/2);
+         if(x==1){
+             count=count+1;
+         }
+     }
+     printf("%d",count);
+     return 0;
+}
