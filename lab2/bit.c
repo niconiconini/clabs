cdbf08907b1dbd8ec6868c3b4d9f33ff4c5b1968
niconiconini <yzxsyh123@126.com>
2019-11-01 20:27:05 -0700

    added a file

---
+++ lab2/bit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{unsigned int i=1;
+unsigned int e=0-1;
+int u;
+    u=0;
+     while(i!=0){
+     i=i<<1;
+     u=u+1;}
+    printf( "i=%d",u);
+    printf("\nu=%u",e);  
+    return 0;
+}
