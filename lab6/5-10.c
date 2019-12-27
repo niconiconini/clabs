11617cf616c9ad65006527026210085bf99d074e
niconiconini <yzxsyh123@126.com>
2019-12-26 22:15:53 -0800

    added a file

--- lab6/5-10.c
+++ lab6/5-10.c
@@ -42,26 +42,55 @@ int main(int argc,char *argv[]){
             case'+':
             push(pop()+pop());
             break;
-            case'-':
+            case '*':
+            push(pop()*pop());
+            break;
+            case '-':
             op2=pop();
             push(pop()-op2);
             break;
-            case'*':
-            push(pop()*pop());
-            break;
-            case'/':
+            case '/':
             op2=pop();
-            if (op2!=0.0)
+            if(op2!=0.0)
             push(pop()/op2);
             else
             printf("error:zero divisor\n");
             break;
             default:
             printf("error:unknown command %s\n",s);
-            argc=1;
+           argv=1;
             break;
         }
     }
     printf("\t%.8g\n",pop());
     return 0;
+}
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+    {
+        printf("error:stack full,can't push %g\n", f);
+    }
+}
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error:stack empty\n");
+        return 0.0;
+    }
+}
+int getop(char s[])
+{
+    if (*s> '9' || *s < '0')
+        return *s;
+    else
+        return NUMBER;
 }
